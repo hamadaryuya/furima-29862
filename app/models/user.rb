6a5079453f2b1642efd8dfc18391da9e3eb94e6a
hamadaryuya <70522981+hamadaryuya@users.noreply.github.com>
2020-09-25 18:14:20 +0900

    user登録のバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname, :family_name, :given_name, :family_name_kana, :given_name_kana, :birth ,presence: true
 end
