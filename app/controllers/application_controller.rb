b4e94f87b7fed4d86b99666ab0e7fe0a31a18432
hamadaryuya <70522981+hamadaryuya@users.noreply.github.com>
2020-09-26 19:26:59 +0900

    deviseのストロングパラメーター設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  bedore_action :configere_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -7,5 +8,9 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
+  end
+  
+  def configere_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:family_name,:given_name,:family_name_kana,:given_name_kana,:birth,])
   end
 end
