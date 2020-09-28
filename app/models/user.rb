a5bb74ad482dffe1afaf5a75b2f8c3fb34cbd034
hamadaryuya <70522981+hamadaryuya@users.noreply.github.com>
2020-09-28 15:33:40 +0900

    ユーザー認証の単体テストコード完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname
     validates :email
-    validates :password, format: { with: /\A[a-z\d]{6,100}+\z/i }
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i }
     validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
     validates :given_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
     validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
