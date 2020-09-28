2fcaadce0ef8d5c7dd7f82ced0fc0fab6714c475
hamadaryuya <70522981+hamadaryuya@users.noreply.github.com>
2020-09-28 15:37:11 +0900

    ユーザー認証までのRubocop適用

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,11 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
     end
   end
-  
+
   def configere_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:family_name,:given_name,:family_name_kana,:given_name_kana,:birth,])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :given_name, :family_name_kana, :given_name_kana, :birth])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,9 @@
 class User < ApplicationRecord
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   with_options presence: true do
     validates :nickname
     validates :email
