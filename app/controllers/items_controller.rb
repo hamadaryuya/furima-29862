363c92069cbb283d6e536d9ef4800241e1927d5f
hamadaryuya <70522981+hamadaryuya@users.noreply.github.com>
2020-09-28 19:30:05 +0900

    Active Storageの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,13 @@
 class ItemsController < ApplicationController
+  def index
+  end
+  
   def new
   end
 
+  private
+
+  def image_params
+    params.require(:item).permit(:image).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,4 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
 end
