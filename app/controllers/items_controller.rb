2bf52514ab8570748961b72660ff69c7c76fda27
hamadaryuya <70522981+hamadaryuya@users.noreply.github.com>
2020-10-01 14:53:38 +0900

    商品出品機能の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: [:index]
   def index
   end
 
@@ -19,5 +20,11 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:name, :discription, :price, :category_id, :area_id, :condition_id, :delivery_fee_id, :ship_days_id, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
