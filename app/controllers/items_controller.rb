e60ab161b10580b3e2576bb1b90e925383db381a
hamadaryuya <70522981+hamadaryuya@users.noreply.github.com>
2020-10-02 17:53:21 +0900

    商品削除機能の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -29,8 +29,11 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.destroy
-    redirect_to root_path
+    if @item.destroy
+      redirect_to root_path
+    else
+      render :show
+    end
   end
 
   private
