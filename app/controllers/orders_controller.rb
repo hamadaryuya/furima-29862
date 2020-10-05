a4d33b4d0fbe3e3308c58d977d79bde45da1ab16
hamadaryuya <70522981+hamadaryuya@users.noreply.github.com>
2020-10-05 13:14:47 +0900

    クレジットカード情報の登録機能

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,20 @@
 class OrdersController < ApplicationController
-
+  before_action :authenticate_user!
+  
   def index
+    
   end
 
   def create
+    binding.pry
+    @item = Item.find_by(id: params[:item_id])
+    
+  end
+
+  private
+
+  def order_params
+    params.permit(:price, :token)
   end
 
 end
--- app/javascript/card.js
+++ app/javascript/card.js
@@ -15,7 +15,19 @@ const pay = ()=> {
     };
     
     Payjp.createToken(card, (status, response) => {
-      
+      if (status == 200) {
+        const token = response.id;
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} type="hidden" name='token'>`;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
+      }
+      document.getElementById("number").removeAttribute("name");
+      document.getElementById("exp_month").removeAttribute("name");
+      document.getElementById("exp_year").removeAttribute("name");
+      document.getElementById("cvc").removeAttribute("name");
+
+      document.getElementById("charge-form").submit();
+      document.getElementById("charge-form").reset();
     })
   })
 }
