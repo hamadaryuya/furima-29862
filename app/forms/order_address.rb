50adb18aa268f44fcdca8cc7edf77908569ea016
hamadaryuya <70522981+hamadaryuya@users.noreply.github.com>
2020-10-06 19:15:07 +0900

    エラーハンドリングの設定

--- app/forms/order_address.rb
+++ app/forms/order_address.rb
@@ -11,6 +11,8 @@ class OrderAddress
     validates :area_id, numericality: { other_than: 1 }
   end
 
+  validates :token, presence: true
+
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
     Address.create(postal_code: postal_code, municipality: municipality, house_number: house_number, building_name: building_name, phone_number: phone_number, area_id: area_id, order_id: order.id)
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,7 @@
 class Order < ApplicationRecord
+
   belongs_to :user
   belongs_to :item
   has_one :address
+  
 end
