4ce5e0333902df4953e238439707e50d6ddc12d3
hamadaryuya <70522981+hamadaryuya@users.noreply.github.com>
2020-10-07 21:48:40 +0900

    before_actionの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,8 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
   before_action :item_find, only: [:index, :create]
+  before_action :move_to_index
+  
 
   def index
     @address = OrderAddress.new
@@ -39,4 +41,9 @@ class OrdersController < ApplicationController
   def item_find
     @item = Item.find(params[:item_id])
   end
+
+  def move_to_index
+    redirect_to root_path unless @item.user_id != current_user.id
+  end
+
 end
